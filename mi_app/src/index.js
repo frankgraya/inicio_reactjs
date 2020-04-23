97408a900fe33d694478dc9c1badfb8773b59ef2
frank <frank@iMac-de-frank.local>
2020-04-22 22:28:30 -0500

    segunda version

--- mi_app/src/index.js
+++ mi_app/src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+//const element = document.createElement('h1')
+//element.innerText = 'Hola  React com estas hola '
+//const container = document.getElementById('root')
+//container.appendChild(element)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const element = <h1>Hello REACT </h1>
+const container = document.getElementById('root')
+
+//esto es jsx
+//ReactDOM.render(___QUE ,DONDE___)
+ReactDOM.render(element,container)
